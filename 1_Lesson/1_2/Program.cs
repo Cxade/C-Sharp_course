68769d0335a32a2294d4dd45e2221790c134addd
Cxade <cxade@mail.ru>
2022-07-11 13:18:29 +0500

    task 1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Является ли первое число квадратом второго
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Write a number b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
