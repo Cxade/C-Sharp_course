863e0a413f77e4fcea0f9415953582a4b5ecddd4
Cxade <cxade@mail.ru>
2022-07-14 17:57:05 +0500

    Task 2_4

---
+++ 2_Lesson/2_4/Program.cs
@@ -0,0 +1,12 @@
+﻿void Method(int a)
+{
+    if (a % 7 == 0 && a % 23 == 0)
+        Console.WriteLine("Кратно");
+    else
+    {
+        Console.WriteLine("Не кратно");
+    }
+}
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Method(N);
