8fc8bf42d2f862cfaa341cb93ca9c5dc2e721fde
Cxade <cxade@mail.ru>
2022-07-14 18:10:36 +0500

    HW 2_1

---
+++ 2_Lesson/Homework/2_1/Program.cs
@@ -0,0 +1,8 @@
+﻿/* Напишите программу, которая принимает на
+вход трёхзначное число и на выходе показывает вторую
+цифру этого числа */
+
+Console.WriteLine("Write a three-digit number a: ");
+int a = int.Parse(Console.ReadLine());
+a = (a / 10) % 10;
+Console.WriteLine(a);
