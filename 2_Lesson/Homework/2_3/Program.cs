f922e8fd95f9cb0483ccaebd4507acec537932d3
Cxade <cxade@mail.ru>
2022-07-15 01:53:17 +0500

    HW 2_3

---
+++ 2_Lesson/Homework/2_3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Write number a: ");
+int a = int.Parse(Console.ReadLine());
+if (a / 100 == 0)
+Console.WriteLine("Третьей цифры нет");
+else
+{
+while (a / 1000 > 0)
+a = a / 10;
+Console.WriteLine(a % 10);
+}
